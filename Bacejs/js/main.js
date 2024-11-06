b75dd930e443645c3e70916c08bea349dee15db4
yuva sri <144772412+yuvasri12@users.noreply.github.com>
2024-11-06 23:07:10 +0530

    Add files via upload

---
+++ Bacejs/js/main.js
@@ -0,0 +1,90 @@
+// console.log("hi dhuddu subscribers")
+// console.log(typeof("diamond"))
+// console.log(typeof(45))
+// // string property
+//  myVariable ="423.09aaa";
+//  //length property
+//  //console.log(myVariable.length)
+//  //console.log(myVariable.charAt(8))
+//  //console.log(myVariable.indexOf("s"))
+// // console.log(myVariable.lastIndexOf("t"))
+// //console.log(myVariable.slice(5,8))// 1st take not take net val
+// //console.log(myVariable.toUpperCase()) for caps katum stinrgs ah
+// //console.log(myVariable.includes("mat"))// boolean val
+// //console.log(myVariable.split(""))// split panni arr nu solum
+// // search somethings another js concepts from online
+
+// //Numbers
+// const myNumber = 42;
+
+// const myFloat = 95.0
+// console.log(myNumber)
+// console.log(myFloat)
+// console.log(myNumber === myFloat)
+// console.log(Number(true)+3)
+// //Number methods
+// console.log(Number.isInteger(420))// t or f num explain
+// console.log(Number.parseFloat(myVariable).toFixed(2));//  vars ahnot print , one num print
+// console.log(typeof myFloat.toString());
+// console.log(parseFloat(myVariable).toFixed(3).toString());
+// // numberisnan, isnan
+// Number.isNaN()
+// isNaN()
+// console.log(Number.isNaN("dhuddu"))//
+// //Math
+// console.log(Math.PI);//333.3333
+// console.log(Math.trunc(Math.PI));//round
+// console.log(Math.round(Math.PI));
+// console.log(Math.ceil(4.5));// val 5 
+// console.log(Math.floor(4.8));//low round val
+// // this all floor methods:ceil trunc
+// console.log(Math.pow(3,4))// 3 to 2 the power of
+// console.log(Math.min(10,3,4,7,8,9,118))
+// console.log(Math.random(2,3,4,6,89,76,78)*10)
+// console.log("yuvasri".charAt(Math.floor(Math.random()*6)+1));
+// const myname = "JAVA DEVELOPER";
+// console.log(myname.charAt(Math.floor(Math.random()*myname.length)));
+// // ithan best solution
+// // main of programmer is reusablity 
+// //IF STATEMENT(conditional statement)
+// // if(condition){
+// //     //run code
+
+// // } else{
+// //     // run diffrent code
+// // }
+
+// // if (viewer){
+// //     reply = `Enjoy ${viewer}`;
+// // } else{
+// // } console.log(reply);
+// let customerISBanned = true;
+// let  viewer ="Dhuddu js full course";
+// let reply;
+// let finance = true;
+// if(customerISBanned){
+//     reply = 'sorry';
+// } else if (viewer && finance){
+//     reply = `Enjoy $ {viewer}`;
+// }else{
+//  reply = 'sorry iam still working';   
+// }
+// console.log(reply);
+let testScore = 88;
+let grade;
+let collegeStudent = true;
+if(testScore = 88)
+    {
+    grade='A';
+}else if (testScore>=80){
+    grade='b';
+}else if{
+    grade ='c';
+} else if (testScore>=60){
+    grade ='D';
+}  else{  if(collegeStudent>=60){
+    grade ='topper'
+}
+    grade = 'f';
+}console.log(grade);
+
